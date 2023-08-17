1ca0bd1a24c1f88db7ebb2fba4eb022c2f001d49
j <137029997+Jagacool@users.noreply.github.com>
2023-08-17 20:50:24 +0530

    Update TodoWrapper.js

--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -3,17 +3,59 @@ import React, { useState } from 'react';
 import { TodoForm } from './TodoForm';
 import { Todo } from './Todo';
 import { v4 as uuidv4 } from 'uuid';
+import { EditTodoForm } from './EditTodoForm';
 
 export const TodoWrapper = () => {
-  // ...
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos([...todos, { id: uuidv4(), ...todo, isEditing: false }]);
+  };
+
+  const deleteTodo = (id) => setTodos(todos.filter((todo) => todo.id !== id));
+
+  const toggleComplete = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const editTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo
+      )
+    );
+  };
+
+  const editTask = (task, id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, task, isEditing: false } : todo
+      )
+    );
+  };
 
   return (
     <div className="TodoWrapper">
       <h1>Get Things Done !</h1>
       <TodoForm addTodo={addTodo} />
-      {todos.map((todo) => (
-        <Todo key={todo.id} todo={todo} /* CRUD functions and status dropdown handler */ />
-      ))}
+      {todos.map((todo) =>
+        todo.isEditing ? (
+          <EditTodoForm key={todo.id} editTodo={editTask} task={todo} />
+        ) : (
+          <Todo
+            key={todo.id}
+            todo={todo}
+            deleteTodo={deleteTodo}
+            editTodo={editTodo}
+            toggleComplete={toggleComplete}
+            editTask={editTask}
+          />
+        )
+      )}
     </div>
   );
 };
