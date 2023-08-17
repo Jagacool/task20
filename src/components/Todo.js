883e1cbf3ff0ca8317a8dc53c3c14f524745d6ff
j <137029997+Jagacool@users.noreply.github.com>
2023-08-17 20:49:35 +0530

    Update Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,28 +1,8 @@
-// Todo.js
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons';
 
-export const Todo = ({ todo, deleteTodo, editTodo, toggleComplete }) => {
-  return (
-    <div className="Todo">
-      <p className={`${todo.completed ? 'completed' : ''}`} onClick={() => toggleComplete(todo.id)}>
-        {todo.task}
-      </p>
-      <div>
-        <FontAwesomeIcon icon={faPenToSquare} onClick={() => editTodo(todo.id)} />
-        <FontAwesomeIcon icon={faTrash} onClick={() => deleteTodo(todo.id)} />
-      </div>
-    </div>
-  );
-};
-
-
-
-// Todo.js
-import React, { useState } from 'react';
-
-export const Todo = ({ todo, deleteTodo, editTodo, toggleComplete }) => {
+export const Todo = ({ todo, deleteTodo, editTodo, toggleComplete, editTask }) => {
   const [statusDropdown, setStatusDropdown] = useState(false);
 
   return (
@@ -36,13 +16,14 @@ export const Todo = ({ todo, deleteTodo, editTodo, toggleComplete }) => {
         {todo.task}
         {statusDropdown && (
           <div className="status-dropdown">
-            <div onClick={() => toggleComplete(todo.id)}>Not Completed</div>
-            <div onClick={() => toggleComplete(todo.id)}>Completed</div>
+            <div onClick={() => editTask('Not Completed', todo.id)}>Not Completed</div>
+            <div onClick={() => editTask('Completed', todo.id)}>Completed</div>
           </div>
         )}
       </p>
       <div>
-        {/* Edit and Delete icons */}
+        <FontAwesomeIcon icon={faPenToSquare} onClick={() => editTodo(todo.id)} />
+        <FontAwesomeIcon icon={faTrash} onClick={() => deleteTodo(todo.id)} />
       </div>
     </div>
   );
